bc2f3f9e08f786eea3a6d6555eea8264187d2d0e
lincontroy <lincolnmunene37@gmail.com>
2024-01-05 19:12:14 +0300

    Fcm issue

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -35,6 +35,7 @@ class NotificationsController extends Controller
     {
         
         $notifications=Notifications::create($request->all());
+        $this->sendPushNotification(env('fcm_token'), $request->subject, $request->body, $id = null);
         return redirect(route('notifications.index'))->with('success', 'Notification created successfully');
     }
 
@@ -69,4 +70,42 @@ class NotificationsController extends Controller
     {
         //
     }
+
+    public function sendPushNotification($fcm_token, $title, $message, $id = null) {  
+    
+        $url = "https://fcm.googleapis.com/fcm/send";            
+        $header = [
+        'authorization: key=' .'wolfsoft1.primewave',
+            'content-type: application/json'
+        ];    
+
+        $postdata = '{
+            "to" : "' . $fcm_token . '",
+                "notification" : {
+                    "title":"' . $title . '",
+                    "text" : "' . $message . '"
+                },
+            "data" : {
+                "id" : "'.$id.'",
+                "title":"' . $title . '",
+                "description" : "' . $message . '",
+                "text" : "' . $message . '",
+                "is_read": 0
+              }
+        }';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $result = curl_exec($ch);    
+        curl_close($ch);
+
+        return $result;
+    }
+
 }
