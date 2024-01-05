14fefcaf3b51fe38d6a13ff0309b908cd2582fb5
lincontroy <lincolnmunene37@gmail.com>
2024-01-05 15:56:12 +0300

    Fixed matches responsive issue

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Matches')
 
 @section('content')
+<div class="container">
     <div class="row">
         <div class="col-lg-12">
 
@@ -9,52 +10,52 @@
                 <div class="card-header">
                     <h5 class="card-title m-0">Matches</h5>
                     <div class="card-tools">
-                        <a href="{{ route('matches.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Add
-                            Match(chelsea,Manchester united)
-                        </a>
+                        <a href="{{ route('matches.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Add Match (Chelsea, Manchester United)</a>
                     </div>
                 </div>
                 <div class="card-body">
-                    <table class="table table-bordered table-striped table-sm">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>League</th>
-                                <th>Home team</th>
-                                <th>Away team</th>
-                                <th>Time</th>
-                                <th>Odds</th>
-                                <th>Created at</th> 
-                                      
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($matches as $key=>$match)
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-sm">
+                            <thead>
                                 <tr>
-                                    <td>{{ $key+1 }}</td>
-                                    <td>{{ $match->league }}</td>
-                                    <td>{{ $match->homeTeam }}</td>
-                                    <td>{{ $match->awayTeam }}</td>
-                                    <td>{{ $match->time }}</td>
-                                    <td>{{ $match->odds }}</td>
-                                    <td>{{ $match->created_at }}</td>
+                                    <th>#</th>
+                                    <th>League</th>
+                                    <th>Home team</th>
+                                    <th>Away team</th>
+                                    <th>Time</th>
+                                    <th>Odds</th>
+                                    <th class="d-none d-md-table-cell">Created at</th> <!-- Hide on smaller screens -->
                                 </tr>
-                            @endforeach
-                            @if (count($matches) == 0)
-                                <tr>
-                                    <td colspan="6">No Match found</td>
-                                </tr>
-                            @endif
-                        </tbody>
-                    </table>
-                    
+                            </thead>
+                            <tbody>
+                                @foreach ($matches as $key=>$match)
+                                    <tr>
+                                        <td>{{ $key+1 }}</td>
+                                        <td>{{ $match->league }}</td>
+                                        <td>{{ $match->homeTeam }}</td>
+                                        <td>{{ $match->awayTeam }}</td>
+                                        <td>{{ $match->time }}</td>
+                                        <td>{{ $match->odds }}</td>
+                                        <td class="d-none d-md-table-cell">{{ $match->created_at }}</td> <!-- Hide on smaller screens -->
+                                    </tr>
+                                @endforeach
+                                @if (count($matches) == 0)
+                                    <tr>
+                                        <td colspan="6">No Match found</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <div class="d-flex justify-content-center">
-                  {{ $matches->links() }}
-                  </div>
+                    {{ $matches->links() }}
+                </div>
             </div>
         </div>
     </div>
+</div>
+
 @endsection
 @section('scripts')
     
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('payments', [App\Http\Controllers\TransactionController::class, 'store'])->name('payments');
 
+Route::post('payments', [App\Http\Controllers\TransactionController::class, 'store'])->name('payments');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
